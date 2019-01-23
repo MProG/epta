fd30e95282ef2f51800df55379aa015b3313dc3e
Shutkov Maksim <shutkov.maksim@mail.ru>
2019-01-23 14:14:31 +0300

    Updated sidekiq config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,12 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:url => uri)
+require 'sidekiq'
+
+Sidekiq.configure_client do |config|
+config.redis = { :size => 1 }
+end
+
+Sidekiq.configure_server do |config|
+config.redis = { :size => 4 }
+end
+
+# uri = URI.parse(ENV["REDISTOGO_URL"])
+# REDIS = Redis.new(:url => uri)
